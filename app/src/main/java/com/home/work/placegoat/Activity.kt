7e0727e88e460c00fa69e6fb0f4a004539e70fdc
sado98 <sado9898kz@gmail.com>
2020-12-14 15:02:29 +0600

    Init project "PlaceGOAT"

---
+++ app/src/main/java/com/home/work/placegoat/Activity.kt
@@ -0,0 +1,11 @@
+package com.home.work.placegoat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/home/work/placegoat/Fragment.kt
@@ -0,0 +1,10 @@
+package com.home.work.placegoat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class Fragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
